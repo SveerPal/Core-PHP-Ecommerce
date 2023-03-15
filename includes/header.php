ac455823e9a49b9b84e9b01a616151f8ca695836
Yash <yashveersoftwere06@gmail.com>
2023-03-15 16:04:31 +0530

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -59,23 +59,7 @@
     <script>
         //var category_id=search=urlAjax ="";
     </script>
-     <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '840430010518400');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=840430010518400&ev=PageView&noscript=1"
- alt="fb" /></noscript>
-<!-- End Meta Pixel Code -->
+    
 
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-7H2DM4DW3Z"></script>
@@ -84,65 +68,16 @@ src="https://www.facebook.com/tr?id=840430010518400&ev=PageView&noscript=1"
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
- gtag('config', 'G-7H2DM4DW3Z');
-  //gtag('config', 'G-7H2DM4DW3Z',{ 'debug_mode': true });
+ gtag('config', 'G-7H2DM4DW3Z__');
+  //gtag('config', 'G-7H2DM4DW3Z__',{ 'debug_mode': true });
 </script>
-<meta name="google-site-verification" content="0y9k7_3CoK1Dne1We3zejbP-0oKYQAkbDEFYyq37fMg" />
-<!-- contact Schema -->
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "LocalBusiness",
-  "name": "AL Pasban",
-  "image": "https://alpasban.com/assets/img/logo/nav-log2.png",
-  "@id": "",
-  "url": "https://alpasban.com/",
-  "telephone": "+91 9311503538",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "B-2 First Floor Chand Bagh",
-    "addressLocality": "New Delhi",
-    "postalCode": "110094",
-    "addressCountry": "IN"
-  }  
-}
-</script>
-<!-- contact Schema end -->
-<!-- Logo Schema -->
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "AL Pasban",
-  "url": "https://alpasban.com/",
-  "logo": "https://alpasban.com/assets/img/logo/nav-log2.png",
-  "sameAs": [
-    "https://www.facebook.com/profile.php?id=100086271502719",
-    "https://www.instagram.com/al_pasban/"
-  ]
-}
-</script>
-<!-- Logo Schema End-->
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KCBVSPW');</script>
-<!-- End Google Tag Manager -->
  
 
 </head>
 
 <body>
-    
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCBVSPW"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-
-
+  
  <!-- Start header area -->
  <header class="header__section color-scheme-2">
         
